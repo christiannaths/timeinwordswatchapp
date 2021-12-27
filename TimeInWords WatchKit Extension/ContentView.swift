e7a5195349173d5025e51c61705e4890640f4798
Christian Naths <christiannaths@gmail.com>
2021-12-27 13:14:33 -0600

    Initial Commit

---
+++ TimeInWords WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeInWords WatchKit Extension
+//
+//  Created by Christian Naths on 2021-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeInWords WatchKit Extension/TimeInWordsApp.swift
@@ -0,0 +1,19 @@
+//
+//  TimeInWordsApp.swift
+//  TimeInWords WatchKit Extension
+//
+//  Created by Christian Naths on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct TimeInWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
